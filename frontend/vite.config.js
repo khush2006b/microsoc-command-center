df0b83b4ab34abf171a054c703c9ec4e16655785
Khush Jain <khush.20243135@mnnit.ac.in>
2025-12-09 18:09:19 +0530

    modified for deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import process from 'process'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+
+  server: {
+    host: true,          // Allow local network access
+    port: 5173           // Local dev port
+  },
+
+  preview: {
+    host: "0.0.0.0",     // REQUIRED: Render needs external binding
+    port: process.env.PORT || 4173,   // REQUIRED: Use Render's assigned port
+  }
 })
